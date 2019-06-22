9f7dc7e24897c532209a32e010a3b7b484c0630f
nikhil <nikhil@sjinnovation.com>
2019-06-22 11:50:43 +0530

    optimization

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const request = require('request');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
+const address = process.argv[2];
 
-geocode('Goa',  (error, data) => {
-    if(error == undefined) {
-        console.log(data);
-        forecast(data, (error, data) => {
-            if(error == undefined) {
-                console.log('Data', data);
-            } else {
-                console.log('Error', error);
-            }
+// console.log(process.argv);
+if(!address) {
+    console.log('Please provide address');
+} else {
+    geocode(address, (error, data) => {
+        if (error == undefined) {
+            console.log(data);
+            forecast(data, (error, data) => {
+                if (error == undefined) {
+                    console.log('Data', data);
+                } else {
+                    console.log('Error', error);
+                }
 
-        })
-    } else {
-        console.log('Error', error);
-    }
+            })
+        } else {
+            console.log('Error', error);
+        }
 
-});
+    });
+}
+
